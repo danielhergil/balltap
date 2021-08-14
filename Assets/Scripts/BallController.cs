5e5e676a5e376b4374c3b355f01f010058ad2445
danielhergil <daniel.hergil@gmail.com>
2021-08-14 23:00:08 +0200

    Game logic and score
    Added game logic, mechanics and score system (not canvas)

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+
+    private Rigidbody ballRb;
+    public float forzeX;
+    public float forzeY;
+    public int points;
+    public bool isFirstTouch = false;
+    public bool aroTouched = false;
+    public int notAroTouchedCounter = 0;
+    public int passes = 0;
+    private float ballRbZAxis;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballRb = GetComponent<Rigidbody>();
+        ballRbZAxis = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && !ballRb.useGravity)
+        {
+            ballRb.useGravity = true;
+            isFirstTouch = true;
+            ballRb.velocity = new Vector3(forzeX, forzeY, ballRb.velocity.z);
+        }
+        if (Input.GetMouseButtonDown(0) && ballRb.useGravity)
+        {
+            forzeX *= -1;
+            ballRb.velocity = new Vector3(forzeX, forzeY, ballRb.velocity.z);
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y, ballRbZAxis);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "dontTouch")
+        {
+            Debug.Log("Has muerto JAJA");
+        }
+        if(collision.gameObject.tag == "aro")
+        {
+            aroTouched = true;
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y, ballRbZAxis);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "basket")
+        {
+            passes++;
+            if(aroTouched)
+            {
+                points += 1;
+                aroTouched = false;
+                notAroTouchedCounter = 0;
+            } else
+            {
+                if (notAroTouchedCounter >= 5)
+                {
+                    points += 300;
+                }
+                points += 5;
+                notAroTouchedCounter++;   
+            }
+        }
     }
 }
---
+++ Assets/Scripts/PlatformPooler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPooler : MonoBehaviour
+{
+    public GameObject[] platforms;
+
+    public BallController theBall;
+    private int theBallPasses = 0;
+    private int platformActive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(theBall.isFirstTouch && theBallPasses == theBall.passes)
+        {
+            StartCoroutine(SetPlatforms());
+        }
+    }
+
+    private IEnumerator SetPlatforms()
+    {
+        theBallPasses++;
+        yield return new WaitForSeconds(0.2f);
+        platforms[platformActive].SetActive(false);
+        float oldPosition = platforms[platformActive].transform.localPosition.y;
+        platformActive = Random.Range(0, 8);
+        Vector3 newPos = platforms[platformActive].transform.localPosition;
+        if(oldPosition < 2.0f)
+        {
+            newPos.y = Random.Range(3.0f, 13.5f);
+        }
+        if (oldPosition > 11.0f)
+        {
+            newPos.y = Random.Range(0.15f, 10.0f);
+        }
+        if (oldPosition > 2.0f && oldPosition < 11.0f)
+        {
+            switch(Random.Range(0, 2))
+            {
+                case 0:
+                    newPos.y = Random.Range(0.15f, oldPosition - 2.0f);
+                    break;
+                case 1:
+                    newPos.y = Random.Range(oldPosition + 2.0f, 13.5f);
+                    break;
+            }
+        }
+        //newPos.y = Random.Range(0.15f, 13.5f);
+        platforms[platformActive].transform.localPosition = newPos;
+        platforms[platformActive].SetActive(true);
+    }
+}
